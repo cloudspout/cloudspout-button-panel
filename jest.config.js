8547a62620aa6af52b579d1694cb7ce43d1dc4b0
Sebastian J <zeeman@zeeman.de>
2021-02-24 22:58:16 -0500

    Inspired by
    https://joelhooks.com/jest-and-github-actions
    and
    https://github.com/stefanbuck/jest-matcher/blob/82d50fdb31d8cdf12afb9c1da8120b4a738c01b1/json-reporter.js

--- jest.config.js
+++ jest.config.js
@@ -6,3 +6,4 @@ const standard = require('@grafana/toolkit/src/config/jest.plugin.config');
 
 // This process will use the same config that `yarn test` is using
 module.exports = standard.jestConfig();
+module.exports.reporters = ["<rootDir>/github-actions-reporter.js"]
